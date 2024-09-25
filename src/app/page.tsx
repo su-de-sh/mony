b1be5fdc1d7c8e1b8a9a3c1a0b78008ca78093fd
haster-tabloids <149601998+haster-tabloids@users.noreply.github.com>
2024-09-25 23:36:05 +0545

    feat: design the database for app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import prisma from "@/lib/db";
 import React from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = async () => {
+  const incomes = await prisma.incomeCategories.findMany();
+  return (
+    <div>
+      {incomes.map((income) => {
+        return <div key={income.id}>{income.name}</div>;
+      })}
+    </div>
+  );
 };
 
 export default Home;
