e388214aef84aac3ce8846bde69872a689cde74a
haster-tabloids <149601998+haster-tabloids@users.noreply.github.com>
2024-09-29 23:40:24 +0545

    feat: put speed insights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import SessionProviders from "@/providers/SessionProviders";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Analytics />
+        <SpeedInsights />
+
         <SessionProviders>{children}</SessionProviders>
       </body>
     </html>
