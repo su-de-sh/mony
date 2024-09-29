ea75c338da41b3650279cb0026afba72fb94025c
haster-tabloids <149601998+haster-tabloids@users.noreply.github.com>
2024-09-29 23:28:11 +0545

    feat: put vercel anaytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import SessionProviders from "@/providers/SessionProviders";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <SessionProviders>{children}</SessionProviders>
       </body>
     </html>
