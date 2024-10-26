fecb9fefb57251a4bc2b9c32ba338fac02df1187
haster-tabloids <149601998+haster-tabloids@users.noreply.github.com>
2024-10-26 14:43:33 +0545

    ref: refactor landing page

---
+++ src/components/atom/Logo/index.tsx
@@ -0,0 +1,18 @@
+export const MonyLogo = () => (
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="20" cy="20" r="20" fill="#FF9F43" />
+    <path
+      d="M11 20C11 15.0294 15.0294 11 20 11C24.9706 11 29 15.0294 29 20C29 24.9706 24.9706 29 20 29"
+      stroke="white"
+      strokeWidth="4"
+      strokeLinecap="round"
+    />
+    <circle cx="20" cy="20" r="4" fill="white" />
+  </svg>
+);
